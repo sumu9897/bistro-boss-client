8943306af3c052e72bbab1a3d87182a836997c37
sumu9897 <sumon.bncd@gmail.com>
2024-12-30 08:38:48 +0600

    Normal Header and Footer Design Done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
 import Footer from "../pages/shared/Footer/Footer"
+import Navbar from "../pages/shared/NavBar/Navbar"
 
 const Main = () => {
   return (
     <div>
+        <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -61,6 +61,13 @@ const Footer = () => {
           </div>
         </nav>
       </footer>
+      <footer className="footer footer-center bg-base-300 text-base-content p-4">
+        <aside>
+          <p>
+            Copyright © {new Date().getFullYear()} - All right reserved by Bistro Boss
+          </p>
+        </aside>
+      </footer>
     </div>
   );
 };
