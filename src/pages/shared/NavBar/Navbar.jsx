deff73ecbc4634765b4c9975b401449ae384f51d
sumu9897 <sumon.bncd@gmail.com>
2024-12-30 09:03:51 +0600

    Banner Design

--- src/pages/shared/NavBar/Navbar.jsx
+++ src/pages/shared/NavBar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
   );
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar fixed z-10 bg-opacity-30 bg-black max-w-screen-xl text-white">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
