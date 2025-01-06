70d5b56f0afc1c482e12d1543edbd9a729b0217e
sumu9897 <sumon.bncd@gmail.com>
2025-01-06 23:39:43 +0600

    Cart location setup

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,8 +1,32 @@
+import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2"
+import {useLocation, useNavigate} from "react-router-dom"
 
 const FoodCard = ({item}) => {
     const {name, image, price, recipe} = item;
+    const {user} = useAuth()
+    const navigate = useNavigate();
+    const location = useLocation();
     const handleAddToCart = food =>{
-      console.log(food)
+      if(user && user.email){
+
+        // TODO: send cart item to the database
+      }else{
+        Swal.fire({
+          title: "You are not Logged In",
+          text: "Please login to add to the cart?",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, login!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // send the user to the login page
+            navigate('/login',{state:{from: location}})
+          }
+        });
+      }
     }
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
