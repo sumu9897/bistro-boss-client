bee24b5d656362a44c8792c273ee027d74fab53e
sumu9897 <sumon.bncd@gmail.com>
2025-01-07 10:07:09 +0600

    refetch cart

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,74 +1,77 @@
 import useAuth from "../../hooks/useAuth";
-import Swal from "sweetalert2"
-import {useLocation, useNavigate} from "react-router-dom"
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useCart from "../../hooks/useCart";
 
-const FoodCard = ({item}) => {
-    const {name, image, price, recipe,_id} = item;
-    const {user} = useAuth()
-    const navigate = useNavigate();
-    const location = useLocation();
-    const axiosSecure = useAxiosSecure();
+const FoodCard = ({ item }) => {
+  const { name, image, price, recipe, _id } = item;
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const axiosSecure = useAxiosSecure();
+  const [, refetch] = useCart();
 
-    const handleAddToCart = food =>{
-      if(user && user.email){
-
-        // TODO: send cart item to the database
-        console.log(user.email, food)
-        const cartItem = {
-          menuId: _id,
-          email: user.email,
-          name,
-          image,
-          price
+  const handleAddToCart = () => {
+    if (user && user.email) {
+      // send cart item to the database
+      // console.log(user.email, food)
+      const cartItem = {
+        menuId: _id,
+        email: user.email,
+        name,
+        image,
+        price,
+      };
+      axiosSecure.post("/carts", cartItem).then((res) => {
+        console.log(res.data);
+        if (res.data.insertedId) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${name} added to your cart`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          // refetch cart to update the cart items
+          refetch();
         }
-        axiosSecure.post('/carts', cartItem)
-        .then(res => {
-          console.log(res.data)
-          if(res.data.insertedId){
-            Swal.fire({
-              position: "top-end",
-              icon: "success",
-              title: `${name} added to your cart`,
-              showConfirmButton: false,
-              timer: 1500
-            });
-
-          }
-        })
-      }else{
-        Swal.fire({
-          title: "You are not Logged In",
-          text: "Please login to add to the cart?",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, login!"
-        }).then((result) => {
-          if (result.isConfirmed) {
-            // send the user to the login page
-            navigate('/login',{state:{from: location}})
-          }
-        });
-      }
+      });
+    } else {
+      Swal.fire({
+        title: "You are not Logged In",
+        text: "Please login to add to the cart?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, login!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // send the user to the login page
+          navigate("/login", { state: { from: location } });
+        }
+      });
     }
+  };
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure>
-        <img
-          src={image}
-          alt={name}
-        />
+        <img src={image} alt={name} />
       </figure>
-      <p className="bg-slate-800 text-white absolute right-0 mr-4 mt-4 px-2 rounded-md">${price}</p>
+      <p className="bg-slate-800 text-white absolute right-0 mr-4 mt-4 px-2 rounded-md">
+        ${price}
+      </p>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-center">
           <button
-          onClick={() => handleAddToCart(item)}
-           className="btn uppercase bg-gray-200 hover:bg-slate-900 text-orange-400  border-orange-400 border-0 border-b-2">Add to Cart</button>
+            onClick={handleAddToCart}
+            className="btn uppercase bg-gray-200 hover:bg-slate-900 text-orange-400  border-orange-400 border-0 border-b-2"
+          >
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
