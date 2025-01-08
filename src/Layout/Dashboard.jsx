b2d89f295c31f7095742c410e9ea58c7df3e120a
sumu9897 <sumon.bncd@gmail.com>
2025-01-08 11:18:12 +0600

    Update

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Outlet } from "react-router-dom"
 
 const Dashboard = () => {
   return (
     <div>
-      
+      <div className="w-64 min-h-full bg-primary">
+        <ul className="menu">
+            <li></li>
+        </ul>
+      </div>
+      <div><Outlet/></div>
+
     </div>
   )
 }
