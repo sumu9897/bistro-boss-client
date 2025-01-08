7db2c0ac838b401a51d0f5281c2f6726181909a4
sumu9897 <sumon.bncd@gmail.com>
2025-01-08 14:26:56 +0600

    update

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,17 +1,24 @@
-import { NavLink, Outlet } from "react-router-dom"
+import { FaShoppingCart } from "react-icons/fa";
+import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div className="flex">
       <div className="w-64 min-h-full bg-primary">
+        <h3 className="uppercase text-center font-bold text-3xl py-8 px-4">Bistro Boss</h3>
         <ul className="menu">
-            <li><NavLink to={'/dashboard/cart'}>My Cart</NavLink></li>
+          <li>
+            <NavLink to={"/dashboard/cart"}>
+              <FaShoppingCart></FaShoppingCart>My Cart
+            </NavLink>
+          </li>
         </ul>
       </div>
-      <div className="flex-1"><Outlet/></div>
-
+      <div className="flex-1">
+        <Outlet />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
